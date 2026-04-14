5c55c52b8be51c792b1936744f7f761e1eafba39
Son Cao <chson230905@gmail.com>
2026-04-14 17:06:16 +0200

    Add some animations for interface, effects for hamburger and Download CV for mobile experience

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ function toggleMenu() {
     menu.classList.toggle("open")
     icon.classList.toggle("open")
 }
+
+function revealOnScroll() {
+    const elements = document.querySelectorAll(".reveal");
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("active");
+            }
+        });
+    }, {
+        threshold: 0.1
+    });
+
+    elements.forEach(el => observer.observe(el));
+}
+
+window.addEventListener("DOMContentLoaded", revealOnScroll);
